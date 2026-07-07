a2f0c25db42f7dca3b967141901a228ccda17931
DevAniela <dismal.damsel@gmail.com>
2026-07-07 13:44:04 +0300

    chore(infrastructure): apply initial database migration for bikes table

---
+++ SmartBikeTracker.Infrastructure/Migrations/20260707100318_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartBikeTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bikes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Battery_Percentage = table.Column<int>(type: "integer", nullable: false),
+                    ChainSensor_RequiresMaintenance = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bikes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bikes");
+        }
+    }
+}
