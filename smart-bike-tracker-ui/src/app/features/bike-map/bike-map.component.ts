57d16fe9fa95aa7d49549b8deaca523acded4621
DevAniela <dismal.damsel@gmail.com>
2026-08-18 20:37:38 +0300

    feat(ui): fetch bikes from api and generate dummy markers on leaflet map

--- smart-bike-tracker-ui/src/app/features/bike-map/bike-map.component.ts
+++ smart-bike-tracker-ui/src/app/features/bike-map/bike-map.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { LeafletModule } from '@bluehalo/ngx-leaflet';
 import * as L from 'leaflet';
+import { BikeApiService } from '../../core/services/bike-api.service';
+import { Bike } from '../../core/models/bike.model';
 
 @Component({
   selector: 'app-bike-map',
@@ -9,7 +11,9 @@ import * as L from 'leaflet';
   templateUrl: './bike-map.component.html',
   styleUrls: ['./bike-map.component.scss']
 })
-export class BikeMapComponent {
+export class BikeMapComponent implements OnInit {
+  private bikeApiService = inject(BikeApiService);
+
   // Configurația de bază pentru Leaflet
   public mapOptions: L.MapOptions = {
     layers: [
@@ -21,4 +25,60 @@ export class BikeMapComponent {
     zoom: 13,
     center: L.latLng(44.4268, 26.1025) // Coordonatele pentru București
   };
+
+  // Acest array va conține markerii
+  public layers: L.Layer[] = [];
+
+  // Pictograma pt bicicletă
+  private bikeIcon = L.icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png', // Icon default momentan
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+  ngOnInit(): void {
+    // Dacă polling-ul nu e pornit global, trebuie să ne asigurăm că îl pornim când intrăm pe hartă
+    this.bikeApiService.startPolling();
+
+    // Ne abonăm la fluxul existent de biciclete din serviciu
+    // Ascultăm schimbările de la polling
+    this.bikeApiService.bikes$.subscribe((bikes) => {
+
+      if (bikes && bikes.length > 0) {
+        // Imediat ce vin de la server, le dăm niște coordonate random din București
+        const bikesWithDummyCoordinates = this.generateDummyCoordinates(bikes);
+  
+        // Transformăm bicicletele în Markere Leaflet
+        this.generateMarkers(bikesWithDummyCoordinates);
+      }
+    });
+  }
+
+  // Funcție care pune markeri pe hartă
+  private generateMarkers(bikes: Bike[]): void {
+    this.layers = bikes.map(bike => {
+      const marker = L.marker([bike.latitude, bike.longitude], {
+        icon: this.bikeIcon,
+        title: bike.name
+      });
+      return marker;
+    });
+  }
+
+  // Helper pentru a genera locații random în București
+  private generateDummyCoordinates(bikes: Bike[]): Bike[] {
+    const minLat = 44.41;
+    const maxLat = 44.45;
+    const minLng = 26.07;
+    const maxLng = 26.12;
+
+    return bikes.map(bike => ({
+      ...bike,
+      latitude: Math.random() * (maxLat - minLat) + minLat,
+      longitude: Math.random() * (maxLng - minLng) + minLng
+    }));
+  }
 }
